3419e5e24481574fab1fd6320488efaa0b3488dc
Wenchang Liu <wenchang.liu@student.manchester.ac.uk>
2020-03-17 22:07:17 +0000

    update network structure

--- demo/networks.py
+++ demo/networks.py
@@ -3,138 +3,146 @@ import torch.nn as nn
 
 # Residual Block
 class ResBlock(nn.Module):
-    def __init__(self, dim, padding_type, norm_layer, activation=nn.ReLU(True), use_dropout=False):
+    def __init__(self, dim, padding_type, norm_layer=nn.BatchNorm2d, activation=nn.ReLU(True)):
         super(ResBlock, self).__init__()
-        self.conv_block = self.build_conv_block(dim, padding_type, norm_layer, activation, use_dropout)
+        self.conv_block = self.build_conv_block(dim, padding_type, norm_layer, activation)
 
-    def build_conv_block(self, dim, padding_type, norm_layer, activation, use_dropout):
+    def build_conv_block(self, dim, padding_type, norm_layer, activation):
         conv_block = []
         p = 0
         if padding_type == 'reflect':
-            conv_block += [nn.ReflectionPad2d(1)]
+            conv_block.append(nn.ReflectionPad2d(1))
         elif padding_type == 'replicate':
-            conv_block += [nn.ReplicationPad2d(1)]
+            conv_block.append(nn.ReplicationPad2d(1))
         elif padding_type == 'zero':
             p = 1
         else:
-            raise NotImplementedError('padding [%s] is not implemented' % padding_type)
+            print("padding type not implemented.")
         
-        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p),
-                        norm_layer(dim), activation]
-        if use_dropout:
-            conv_block += [nn.Dropout(0.5)]
+        conv_block.extend([nn.Conv2d(dim, dim, kernel_size=3, padding=p),
+                    norm_layer(dim), activation])
         
         p = 0
         if padding_type == 'reflect':
-            conv_block += [nn.ReflectionPad2d(1)]
+            conv_block.append(nn.ReflectionPad2d(1))
         elif padding_type == 'replicate':
-            conv_block += [nn.ReplicationPad2d(1)]
+            conv_block.append(nn.ReplicationPad2d(1))
         elif padding_type == 'zero':
             p = 1
         else:
-            raise NotImplementedError('padding [%s] is not implemented' % padding_type)
-        
-        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p),
-                        norm_layer(dim), activation]
+            print("padding type not implemented.")
+        conv_block.extend([nn.Conv2d(dim, dim, kernel_size=3, padding=p),
+                    norm_layer(dim), activation])
         
         return nn.Sequential(*conv_block)
     
     def forward(self, x):
-        out = x + self.conv_block(x)
-        return out
+        return x + self.conv_block(x)
 
 
 # G1, global generator
 class GlobalGenerator(nn.Module):
     def __init__(self, input_nc, output_nc, ngf=64, 
                 n_downsampling=3, n_blocks=6, norm_layer=nn.BatchNorm2d, padding_type='reflect'):
-        assert(n_blocks >=0)
+        
         super(GlobalGenerator, self).__init__()
-        activation = nn.ReLU(True)
 
         model = [nn.ReflectionPad2d(3), nn.Conv2d(input_nc, ngf, kernel_size=7, padding=0),
-                norm_layer(ngf), activation]
+                norm_layer(ngf), nn.ReLU(True)]
+        
         # downsample
         for i in range(n_downsampling):
-            mult = 2**i
-            model += [nn.Conv2d(ngf*mult, ngf*mult*2, kernel_size=3, stride=2, padding=1),
-                    norm_layer(ngf*mult*2), activation]
+            model.extend([nn.Conv2d(ngf*(2**i), ngf*(2**(i+1)), kernel_size=3, stride=2, padding=1),
+                    norm_layer(ngf*(2**(i+1))), nn.ReLU(True)])
+            
         # resnet blocks
         mult = 2**n_downsampling
         for i in range(n_blocks):
-            model += [ResBlock(ngf*mult, padding_type=padding_type, 
-                            activation=activation, norm_layer=norm_layer)]
+            model.append(ResBlock(ngf*mult, padding_type=padding_type, 
+                            activation=nn.ReLU(True), norm_layer=norm_layer))
+            
         # upsample
         for i in range(n_downsampling):
             mult = 2**(n_downsampling - i)
-            model += [nn.ConvTranspose2d(ngf*mult, int(ngf*mult/2), 
-                                        kernel_size=3, stride=2, 
-                                        padding=1, output_padding=1),
-                    norm_layer(int(ngf*mult/2)), activation]
-        model += [nn.ReflectionPad2d(3), nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0), nn.Tanh()]
+            model.extend([nn.ConvTranspose2d(ngf*mult, int(ngf*mult/2), kernel_size=3, stride=2, padding=1, output_padding=1), 
+                        norm_layer(int(ngf*mult/2)), nn.ReLU(True)])
+        
+        model.append(nn.ReflectionPad2d(3))
+        model.append(nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0))
+        model.append(nn.Tanh())
         self.model = nn.Sequential(*model)
 
-    def forward(self, input):
-        return self.model(input)
-
+    def forward(self, x):
+        return self.model(x)
 
 # local enhancer
 class LocalEnhancer(nn.Module):
-    def __init__(self, input_nc, output_nc, model_global, ngf=32, n_downsample_global=3, n_blocks_gobal=9,
+    def __init__(self, input_nc, output_nc, model_global, ngf=32, n_downsample_global=3, n_blocks_gobal=6,
                 n_local_enhancers=1, n_blocks_local=3, norm_layer=nn.BatchNorm2d, padding_type='reflect'):
         super(LocalEnhancer, self).__init__()
         self.n_local_enhancers = n_local_enhancers
         self.model_global = model_global.model
 
         # global generator model
-        ngf_global = ngf * (2**n_local_enhancers)
+        mult = 2**n_local_enhancers
+        ngf_global = ngf * mult
         # model_global = GlobalGenerator(input_nc, output_nc, ngf_global, n_downsample_global, n_blocks_gobal, norm_layer).model
         model_global = [self.model_global[i] for i in range(len(self.model_global)-3)] # get rid of final conv layers
         self.model = nn.Sequential(*model_global)
+
+        self.downsample = nn.AvgPool2d(3, stride=2, padding=[1, 1], count_include_pad=False)
         
         # local enhancer layers
         for n in range(1, n_local_enhancers+1):
             # downsample
-            ngf_global = ngf * (2**(n_local_enhancers-n))
-            model_downsample = [nn.ReflectionPad2d(3), nn.Conv2d(input_nc, ngf_global, kernel_size=7, padding=0),
-                                norm_layer(ngf_global), nn.ReLU(True),
-                                nn.Conv2d(ngf_global, ngf_global*2, kernel_size=3, stride=2, padding=1),
+            mult = 2**(n_local_enhancers-n)
+            ngf_global = ngf * mult
+            model_downsample = []
+            model_downsample_block1 = [nn.ReflectionPad2d(3), nn.Conv2d(input_nc, ngf_global, kernel_size=7, padding=0),
+                                norm_layer(ngf_global), nn.ReLU(True)]
+            model_downsample_block2 = [nn.Conv2d(ngf_global, ngf_global*2, kernel_size=3, stride=2, padding=1),
                                 norm_layer(ngf_global*2), nn.ReLU(True)]
+            model_downsample.extend(model_downsample_block1)
+            model_downsample.extend(model_downsample_block2)
+            
             # residul blocks
             model_upsample = []
             for i in range(n_blocks_local):
-                model_upsample += [ResBlock(ngf_global * 2, padding_type=padding_type, norm_layer=norm_layer)]
+                model_upsample.append(ResBlock(ngf_global * 2, padding_type=padding_type, norm_layer=norm_layer))
             
             # upsample
-            model_upsample += [nn.ConvTranspose2d(ngf_global * 2, ngf_global, 
+            model_upsample.extend([nn.ConvTranspose2d(ngf_global * 2, ngf_global, 
                                                 kernel_size=3, stride=2, padding=1, output_padding=1),
-                                norm_layer(ngf_global), nn.ReLU(True)]
+                                norm_layer(ngf_global), nn.ReLU(True)])
+            
             # final convolution
             if n == n_local_enhancers:
-                model_upsample += [nn.ReflectionPad2d(3), nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0), nn.Tanh()]
+                model_upsample.extend([nn.ReflectionPad2d(3), nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0), nn.Tanh()])
             
-            setattr(self, 'model'+str(n)+'_1', nn.Sequential(*model_downsample))
-            setattr(self, 'model'+str(n)+'_2', nn.Sequential(*model_upsample))
-        
-        self.downsample = nn.AvgPool2d(3, stride=2, padding=[1, 1], count_include_pad=False)
+            self.le_downsample = nn.Sequential(*model_downsample)
+            self.le_upsample =  nn.Sequential(*model_upsample)
+
     
-    def forward(self, input):
+    def forward(self, x):
         # create input pyramid
-        input_downsampled = [input]
+        input_downsampled = [x]
         for i in range(self.n_local_enhancers):
             input_downsampled.append(self.downsample(input_downsampled[-1]))
         
         # output at coarest level
         output_prev = self.model(input_downsampled[-1])
         # build up one layer at a time
+        model_downsample = self.le_downsample
+        model_upsample = self.le_upsample
         for n_local_enhancers in range(1, self.n_local_enhancers+1):
-            model_downsample = getattr(self, 'model'+str(n_local_enhancers)+'_1')
-            model_upsample = getattr(self, 'model'+str(n_local_enhancers)+'_2')            
-            input_i = input_downsampled[self.n_local_enhancers-n_local_enhancers]            
+            # downsample            
+            input_i = input_downsampled[self.n_local_enhancers-n_local_enhancers]
+            # upsample
             output_prev = model_upsample(model_downsample(input_i) + output_prev)
         return output_prev
 
 
+import torch.nn.functional as F
 class SpadeBlock(nn.Module):
     def __init__(self, norm_nc, label_nc=3):
         super().__init__()
@@ -144,7 +152,7 @@ class SpadeBlock(nn.Module):
         nhidden = 128
 
         ks = 3 # kernel size
-        pw = ks // 2 # padding
+        pw = 1 # padding
         self.shared_net = nn.Sequential(
             nn.Conv2d(label_nc, nhidden, kernel_size=ks, padding=pw),
             nn.ReLU()
@@ -206,15 +214,11 @@ class SpadeResBlock(nn.Module):
 
 import torch.nn.functional as F
 class SpadeGenerator(nn.Module):
-    def __init__(self, plusStyle=False):
+    def __init__(self):
         super().__init__()
-        self.plusStyle = plusStyle
-        if self.plusStyle:
-            pass
-        else:
-            # hardcoded, took from paper
-            self.fc = nn.Linear(256, 1024*2*4)
-        
+        # hardcoded, took from paper
+        self.fc = nn.Linear(256, 1024*2*4)
+    
         self.spadeRes0 = SpadeResBlock(1024, 1024)
         self.spadeRes1 = SpadeResBlock(1024, 1024)
         self.spadeRes2 = SpadeResBlock(1024, 1024)
@@ -229,13 +233,10 @@ class SpadeGenerator(nn.Module):
     def forward(self, input, z=None):
         seg = input
 
-        if self.plusStyle:
-            pass
-        else:
-            if z is None:
-                z = torch.randn(input.size(0), 256,
-                                dtype=torch.float32)
-            x = self.fc(z)
+        if z is None:
+            z = torch.randn(input.size(0), 256,
+                            dtype=torch.float32)
+        x = self.fc(z)
         
         b, c, h, w = seg.size()
         # hardcoded as the paper said
